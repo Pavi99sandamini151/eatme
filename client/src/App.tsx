dfbba9b192ac434fe4eb3505dd516dda49b165cd
Pavithra sandamini <pavithra.sandamini@q4us.dev>
2023-08-15 20:54:04 +0530

    signup done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import './assets/styles/main.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login/Login';
+import Signup from './pages/signup/Signup';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         {/* <Route path="/signup" element={ <Signup /> } /> */}
       </Routes>
     </BrowserRouter>
--- client/src/pages/AppRouter.tsx
+++ client/src/pages/AppRouter.tsx
@@ -3,13 +3,14 @@ import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login/Login';
 import { ProtectedPage } from './ProtectedPage';
+import Signup from './signup/Signup';
 
 const AppRouter = () => {
   return (
     <HashRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route element={<ProtectedPage userRole="admin" />}>
           <Route path="/dashboard" element={<Home />} />
         </Route>
